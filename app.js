7ea557dec81bc35c6bc9f21a0835917f45c7b94e
task1 <eltacem@code.edu.az>
2022-04-06 01:00:40 +0400

    'com-2'

---
+++ app.js
@@ -0,0 +1,18 @@
+
+let first = document.getElementById("first")
+
+let a = confirm("Do you like black");
+
+if(a == true)
+{
+    first.style.backgroundColor = "black"
+}
+
+
+let x = prompt("How old are you?");
+
+
+    if (x <18)
+    {
+    alert("Yas Heddi")
+    }
